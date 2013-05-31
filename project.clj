23cdfbe9cbf7fe1bc03e14ea5de9c4de810d5e96
Brendan Younger <byounger@roomkey.com>
2013-05-31 14:55:22 -0400

    Bump clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject matchure "0.11.0"
+(defproject brendanyounger/matchure "0.11.0"
   :description "Idiomatic and powerful pattern matching in clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]])
